6e0b055cffdad9fa03642cbbf45c3cf362d19d10
Ebrar Kaya <ebrkaya9852@gmail.com>
2024-10-31 16:23:28 +0300

    Add files via upload

---
+++ degiskenornek.py
@@ -0,0 +1,18 @@
+name = "ebrar"
+surname = 'kaya'
+
+a = '50'           # sözel olarak 50 değeri tanımladık
+b = "50"           # sözel (string) olarak 50 değeri tanımladık.
+
+1yas => hatalı
+yas1 => geçerli
+_yas => geçerli
+
+firstName = 'ebrar'
+FirstName = 'gokcen'
+
+x = 1             # int
+y = 2.3           # float
+name = 'ebrar'    # string
+isStudent = True  # bool
+
